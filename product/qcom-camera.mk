581493bdc336e753a0b861b6cb48492020006a45
chaubeyprateek <chaubeyprateek@gmail.com>
2016-10-18 17:20:48 +0200

    Fixes Video recording

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -24,3 +24,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Disable all camera debug
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.hal.debug.mask=0
+
+# Enable low power video mode for 4K encode
+PRODUCT_PROPERTY_OVERRIDES += \
+    vidc.debug.perf.mode=2 \
+    vidc.enc.dcvs.extra-buff-count=2
+
+# Video Recording Blobs
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.camera.cpp.duplication=false= \
+    camera.disable_zsl_mode=1= \
+    media.camera.ts.monotonic=0
